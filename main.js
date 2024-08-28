b85d2518e5ec5fd19cd93bb37619637cb356ff41
Neo-Lion-Studio <xiiclub.cn@gmail.com>
2024-08-28 21:58:45 +0800

    main.js

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ import { Game } from './game.js';
 import { Menu } from './menu.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+    console.log("DOM loaded");
     const gameContainer = document.getElementById('gameContainer');
     const menu = new Menu(gameContainer);
     menu.init();
+    console.log("Menu initialized");
 });
