91dc003956b31af7d6712adfbeec660ce5f87cc6
Neo-Lion-Studio <xiiclub.cn@gmail.com>
2024-08-28 21:57:44 +0800

    ball.js

--- ball.js
+++ ball.js
@@ -1,9 +1,11 @@
 export class Ball {
     constructor(element, gameSize) {
-        this.entity = element;
+        this.element = element;
         this.speedY = 5;
-        this.x = element.offsetLeft;
-        this.y = element.offsetTop;
+        this.width = 45;
+        this.height = 45;
+        this.x = parseInt(element.style.left);
+        this.y = parseInt(element.style.top);
         this.currentFrame = 0;
         this.startAnimation();
     }
@@ -11,18 +13,26 @@ export class Ball {
     update(gravity, gameSize) {
         this.speedY += gravity;
         this.y += this.speedY;
-        this.y = Math.max(0, Math.min(this.y, gameSize - 45));
-        this.entity.style.top = this.y + 'px';
+        this.y = Math.max(0, Math.min(this.y, gameSize - this.height));
+        this.element.style.top = this.y + 'px';
     }
 
     jump(gravity) {
         this.speedY = -10 * gravity;
     }
 
+    getX() {
+        return this.x;
+    }
+
+    getY() {
+        return this.y;
+    }
+    
     startAnimation() {
         setInterval(() => {
             this.currentFrame = (this.currentFrame + 1) % 4;
-            this.entity.className = `ball ball${this.currentFrame + 1}`;
+            this.element.className = `ball ball${this.currentFrame + 1}`;
         }, 50);
     }
 }
